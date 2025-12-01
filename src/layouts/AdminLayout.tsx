b39e7d073a0aceead9c86030170e0d069ab3d4a6
liemlv2003 <levanliem2003@gmail.com>
2025-12-01 18:50:34 +0700

    chore: update admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -4,17 +4,17 @@ import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
    return (
-      <div className="min-h-screen flex bg-gray-50 dark:bg-gray-950">
+      <div className="min-h-screen flex bg-gray-50 dark:bg-gray-950 overflow-x-hidden">
          <header className="fixed top-0 left-0 w-full z-30">
             <Header />
          </header>
 
-         <div className="fixed top-16 left-0 bottom-0 w-64">
+         <div className="fixed top-16 left-0 bottom-0 w-64 z-20">
             <AdminSidebar />
          </div>
 
-         <div className="flex-1 ml-64 pt-16">
-            <main className="p-6">
+         <div className="flex-1 ml-64 pt-16 overflow-x-hidden">
+            <main className="p-6 overflow-x-hidden">
                <Outlet />
             </main>
          </div>
